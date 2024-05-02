a0bdb7b0b32f68365fa162b23625caa703bd16b7
Tony Eder <tonyeder11@gmail.com>
2024-05-02 14:08:54 -0700

    databases now visible after flask db init, flask db migrate -m initial migration and flask db upgrade

--- server/models.py
+++ server/models.py
@@ -6,14 +6,11 @@ from config import db, bcrypt
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String, unique = True)
-    _password_hash = db.Column(db.String, nullable = False)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String, nullable=False)
     image_url = db.Column(db.String)
     bio = db.Column(db.String)
-
-    def __repr__(self):
-        return f'User {self.username}, ID {self.id}'
     
     @hybrid_property
     def password_hash(self):
@@ -21,13 +18,27 @@ class User(db.Model, SerializerMixin):
 
     @password_hash.setter
     def password_hash(self, password):
-        password_hash = bcrypt.generate_password_hash(password.encode('utf-8')
+        password_hash = bcrypt.generate_password_hash(password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
 
     def authenticate(self, password):
         return bcrypt.check_password_hash(
             self._password_hash, password.encode('utf-8')
         )
+    
+    recipes = db.relationship('Recipe', back_populates='user')
+
+    def __repr__(self):
+        return f'User {self.username}, ID {self.id}'
 
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable = False)
+    instructions = db.Column(db.String(50), nullable = False)
+    minutes_to_complete = db.Column(db.Integer)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
     
